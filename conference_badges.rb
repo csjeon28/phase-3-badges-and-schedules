bf5dfef02970282e62a4ce84f8b3d51acc6c21b3
Christine Jeon <csjeon28@gmail.com>
2021-06-14 00:43:00 -0400

    commit tree 1b91d46389151043159e960df20907e15ea68b78

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(names)
+    names.map do |name|
+     "Hello, my name is #{name}."
+    end
+end
+
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, index| 
+     "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+end
+
+
+def printer(speakers)
+    batch_badge_creator(speakers).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(speakers).each do |rooms|
+        puts rooms
+    end
+end
